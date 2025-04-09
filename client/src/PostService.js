5a247d5b1adb04eef7587f6918e2cc6853714f33
redpanda5522 <christopherpaonessa5522@gmail.com>
2025-04-09 13:25:02 -0400

    Frontend

---
+++ client/src/PostService.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const url = "http://localhost:5000/api/posts/";
+
+class PostService {
+ // Get Posts
+  static async getPosts() {
+    try {
+      const res = await axios.get(url);
+      const data = res.data;
+      return data.map((post) => ({
+        ...post,
+        createdAt: new Date(post.createdAt),
+      }));
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
+
+  // Create Post
+  static insertPost(text) {
+    return axios.post(url, { text: text });
+  }
+
+  // Delete Post
+  static deletePost(id) {
+    return axios.delete(`${url}${id}`);
+  }
+}
+
+export default PostService;
